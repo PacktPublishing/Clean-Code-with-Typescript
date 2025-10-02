55ddd55bcb14d7a485eada5ee1ace4360ee4db63
Rukee Ojigbo <44168306+Rukeeo1@users.noreply.github.com>
2025-10-02 14:16:36 +0200

    chore: update controller to use the relevant types... api request works

--- ch10/devjobs/apps/devjobs-backend/src/app/users/users.controller.ts
+++ ch10/devjobs/apps/devjobs-backend/src/app/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post,  Body } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import type {
+  CreateUser,
+  UserResponse,
+} from "../../../../../libs/api-types/model";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  register(@Body() user: CreateUser): Promise<UserResponse> {
+    return this.usersService.register(user);
+  }
+}
+
+// wire user service to user controller
+// make a curl command -> test in post man ->
+// copy curl command
--- ch10/devjobs/apps/devjobs-backend/src/app/users/users.service.ts
+++ ch10/devjobs/apps/devjobs-backend/src/app/users/users.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { randomUUID } from "crypto";
+import {
+  CreateUser,
+  User,
+  UserResponse,
+} from "../../../../../libs/api-types/model";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = []; // Now using internal type for secure storage
+
+  async register(user: CreateUser): Promise<UserResponse> {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    const newUser = {
+      id: randomUUID(),
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      companyId: user.companyId,
+      password: hashedPassword,
+    };
+
+    this.users.push(newUser);
+
+    const { password, ...userWithoutPassword } = newUser;
+
+    return userWithoutPassword;
+  }
+}
