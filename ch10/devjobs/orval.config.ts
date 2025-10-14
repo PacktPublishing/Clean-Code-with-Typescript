a09d78fe4772173f31329559943592dec5a7ce7f
Rukee Ojigbo <44168306+Rukeeo1@users.noreply.github.com>
2025-10-14 18:58:08 +0200

    chore: set up frontend

--- ch10/devjobs/orval.config.ts
+++ ch10/devjobs/orval.config.ts
@@ -1,10 +1,18 @@
+
 export default {
-    devjobs: {
-      input: './apps/devjobs-backend/src/openapi.yml',
-      output: {
-        schemas: './libs/api-types/model',  
-        target: './libs/api-types',
+  devjobs: {
+    input: './apps/devjobs-backend/src/openapi.yml',
+    output: {
+      mode: 'tags-split',
+      target: './libs/api-client/src/generated',
+      schemas: './libs/api-types/model',
+      client: 'react-query',
+      override: {
+        mutator: {
+          path: './libs/api-client/src/axios-instance.ts',
+          name: 'customInstance',
+        },
       },
     },
-  };
-  
+  },
+};
